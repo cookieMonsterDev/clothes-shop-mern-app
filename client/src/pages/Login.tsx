4de80b0034958c5a966a82b932e5ea4339a20f18
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-10-10 19:08:58 +0300

    chore: add Login & Register pages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,12 +3,76 @@ import styled from 'styled-components'
 
 const Login = () => {
   return (
-    <Container>Login</Container>
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Button>Login</Button>
+          <TLink>DO NOT YOU REMEMBER THE PASSWORD?</TLink>
+          <TLink>CREATE A NEW ACCOUNT</TLink>
+        </Form>
+      </Wrapper>
+    </Container>
   )
 }
 
 export default Login
 
 const Container = styled.div`
-  
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  margin: 20px 10px 0 0;
+  width: 80%;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  margin: 10px 0 0 0;
+  width: 20%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  font-size: 20px;
+`;
+
+const TLink = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
 `
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,14 +1,79 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Register = () => {
   return (
-    <Container>Register</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Agreement>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, porro
+            tempore, distinctio non laborum nihil ea accusamus architecto
+            quaerat quod deserunt fuga!
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Register
+export default Register;
 
 const Container = styled.div`
-  
-`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  margin: 20px 10px 0 0;
+  width: 80%;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0;
+`;
+
+const Button = styled.button`
+  width: 20%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  font-size: 20px;
+`;
